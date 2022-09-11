03d0bce9e2dea006663e4c1d41089afb29b20d67
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-09-12 01:22:14 +0200

    12092022 commit

---
+++ formating_in_python.py
@@ -0,0 +1,10 @@
+### “New Style” String Formatting (str.format)
+
+# https://realpython.com/python-string-formatting/#2-new-style-string-formatting-strformat
+# https://realpython.com/python-formatted-output/
+# https://docs.python.org/3/library/stdtypes.html#str.format
+"""
+
+
+
+"""
