a68af8340f7d98771fc312af3e85cb14f2cb5433
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-14 00:29:35 +0100

    13112022 commit

---
+++ 13_SQL_problems/03_CREATE_TABLE.py
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS 'store', 'category' (
+
+    'id' INT NOT NULL,
+    'name' VARCHAR(255) NULL,
+    PRIMARY KEY ('id')
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
