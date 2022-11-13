d689162783edde20e05d4db97d1d74ca4e4dd303
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-13 21:59:32 +0100

    13112022 commit

--- 13_SQL_problems/02_OUTER_JOIN.py
+++ 13_SQL_problems/02_OUTER_JOIN.py
@@ -43,3 +43,23 @@ SELECT c.id, c.name FROM category AS c
         WHERE cg.category_id = c.id)
 ;
 
+# ЗАДАНИЕ 15
+# Выведите список источников, из которых не было клиентов,
+# либо клиенты пришедшие из которых не совершали заказов или
+# отказывались от заказов. Под клиентами, которые отказывались
+# от заказов, необходимо понимать клиентов, у которых есть заказы,
+# которые на момент выполнения запроса находятся в
+# состоянии 'rejected'. В запросе необходимо использовать
+# оператор UNION для объединения выборок по разным условиям.
+
+SELECT source.name AS source_name
+FROM source
+WHERE NOT EXISTS (SELECT * FROM client
+					WHERE client.source_id = source.id)
+
+UNION
+
+SELECT source.name FROM source
+	INNER JOIN client ON client.source_id = source.id
+	INNER JOIN sale ON sale.client_id = client.id
+	INNER JOIN status ON status.id = sale.status_id WHERE status.name = 'rejected';
