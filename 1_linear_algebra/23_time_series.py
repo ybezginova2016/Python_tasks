6daa6763902ef3eaa170ae3b82835635b0634d4c
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-20 21:53:19 +0100

    20112022 commit

--- 1_linear_algebra/23_time_series.py
+++ 1_linear_algebra/23_time_series.py
@@ -16,8 +16,6 @@ print(data.info())
 
 import pandas as pd
 
-# data = pd.read_csv('/datasets/energy_consumption.csv')
-
 # OPTION 1 #
 from datetime import datetime
 dateparse = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
@@ -26,5 +24,56 @@ data = pd.read_csv('/datasets/energy_consumption.csv', parse_dates=['Datetime'],
 print(data.info())
 
 # OPTION 2
+
+data = pd.read_csv('/datasets/energy_consumption.csv')
 data['Datetime'] = pd.to_datetime(data['Datetime'])
 print(data.info())
+
+### 2. Установите индекс таблицы равным столбцу Datetime.
+# В документации Pandas выберите любой способ установки индекса.
+
+# Установить индекс таблицы можно двумя способами:
+
+# функции read_csv() передать аргумент index_col (от англ. index
+# column, «индексирующий столбец»)
+import pandas as pd
+data = pd.read_csv('/datasets/energy_consumption.csv', parse_dates=[0], index_col=0)
+print(data.info())
+
+# вызвать функцию df.set_index().
+import pandas as pd
+data = pd.read_csv('/datasets/energy_consumption.csv', parse_dates=[0])
+data.set_index('Datetime', inplace = True)
+print(data.info())
+
+### 3. Чтобы проверить, в хронологическом ли порядке расположены даты
+# и время, посмотрите атрибут индекса таблицы is_monotonic (англ.
+# «монотонный»). Если порядок соблюдён, атрибут вернёт True, если
+# нет — False. Отсортируйте индекс таблицы. Метод найдите в
+# документации. Напечатайте на экране значение атрибута is_monotonic.
+# Затем вызовом функции info() выведите на экран общую информацию
+# о таблице.
+
+# Индекс сортируется методом sort_index()
+# (англ. «отсортировать индекс»).
+
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.is_monotonic.html
+import pandas as pd
+data = pd.read_csv('/datasets/energy_consumption.csv', index_col=[0], parse_dates=[0])
+
+data.sort_index(ascending=True, axis=0,
+               level=None, inplace=True)
+print(data.index.is_monotonic)
+print(data.info())
+
+# Output
+# True
+# <class 'pandas.core.frame.DataFrame'>
+# DatetimeIndex: 145366 entries, 2002-01-01 01:00:00 to 2018-08-03 00:00:00
+# Data columns (total 1 columns):
+#  #   Column   Non-Null Count   Dtype
+# ---  ------   --------------   -----
+#  0   PJME_MW  145366 non-null  float64
+# dtypes: float64(1)
+# memory usage: 2.2 MB
+# None
