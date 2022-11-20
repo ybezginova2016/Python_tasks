bd3d9a7abe9d74316b5aa8f406f39334f004b906
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-20 22:00:48 +0100

    20112022 commit

--- 1_linear_algebra/23_time_series.py
+++ 1_linear_algebra/23_time_series.py
@@ -29,11 +29,21 @@ data = pd.read_csv('/datasets/energy_consumption.csv')
 data['Datetime'] = pd.to_datetime(data['Datetime'])
 print(data.info())
 
+# Output
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 145366 entries, 0 to 145365
+# Data columns (total 2 columns):
+#  #   Column    Non-Null Count   Dtype
+# ---  ------    --------------   -----
+#  0   Datetime  145366 non-null  datetime64[ns]
+#  1   PJME_MW   145366 non-null  float64
+# dtypes: datetime64[ns](1), float64(1)
+# memory usage: 2.2 MB
+# None
+
 ### 2. Установите индекс таблицы равным столбцу Datetime.
 # В документации Pandas выберите любой способ установки индекса.
-
 # Установить индекс таблицы можно двумя способами:
-
 # функции read_csv() передать аргумент index_col (от англ. index
 # column, «индексирующий столбец»)
 import pandas as pd
@@ -45,6 +55,17 @@ import pandas as pd
 data = pd.read_csv('/datasets/energy_consumption.csv', parse_dates=[0])
 data.set_index('Datetime', inplace = True)
 print(data.info())
+
+# Output
+# <class 'pandas.core.frame.DataFrame'>
+# DatetimeIndex: 145366 entries, 2002-12-31 01:00:00 to 2018-01-02 00:00:00
+# Data columns (total 1 columns):
+#  #   Column   Non-Null Count   Dtype
+# ---  ------   --------------   -----
+#  0   PJME_MW  145366 non-null  float64
+# dtypes: float64(1)
+# memory usage: 2.2 MB
+# None
 
 ### 3. Чтобы проверить, в хронологическом ли порядке расположены даты
 # и время, посмотрите атрибут индекса таблицы is_monotonic (англ.
@@ -77,3 +98,33 @@ print(data.info())
 # dtypes: float64(1)
 # memory usage: 2.2 MB
 # None
+
+### 4.Из временного ряда выделите данные с января по июнь 2018 года.
+# Даты во временных рядах можно указывать в срезах. В прекоде выбраны
+# значения с 2016 по 2017 год включительно.
+import pandas as pd
+
+data = pd.read_csv('/datasets/energy_consumption.csv', index_col=[0], parse_dates=[0])
+data.sort_index(inplace=True)
+# data = data['2016':'2017']
+data = data['2018-01': '2018-06']
+print(data.info())
+# <class 'pandas.core.frame.DataFrame'>
+# DatetimeIndex: 4343 entries, 2018-01-01 00:00:00 to 2018-06-30 23:00:00
+# Data columns (total 1 columns):
+#  #   Column   Non-Null Count  Dtype
+# ---  ------   --------------  -----
+#  0   PJME_MW  4343 non-null   float64
+# dtypes: float64(1)
+# memory usage: 67.9 KB
+# None
+
+# 5. Постройте график временного ряда.
+import pandas as pd
+
+data = pd.read_csv('/datasets/energy_consumption.csv', index_col=[0], parse_dates=[0])
+data.sort_index(inplace=True)
+data = data['2018-01':'2018-06']
+
+# plotting
+data['2018-01':'2018-06'].plot()
