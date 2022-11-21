09791578116a66e15d5c9bfd093a48e460e68eca
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-21 06:01:27 +0100

    21112022 commit

--- 1_linear_algebra/30_quality_metrics.py
+++ 1_linear_algebra/30_quality_metrics.py
@@ -30,7 +30,6 @@ train, test = train_test_split(data, shuffle=False, test_size=0.2)
 print("Средний объём электропотребления в день:", test['PJME_MW'].mean())
 
 pred_median = np.ones(test.shape) * train['PJME_MW'].median()
-
 mae = mean_absolute_error(pred_median, test['PJME_MW'])
 print("MAE:", mae)
 
@@ -39,3 +38,37 @@ print("MAE:", mae)
 # MAE: 96625.08333333333
 
 # 745523 / 96625 ~ 13%, which is good.
+
+#### 2 ####
+# Для предсказания значений вызовите метод shift()
+# у тестовой выборки. Чтобы заполнить первое значение,
+# изучите, как работает атрибут iloc (от
+# англ. integer location, «обращение
+# по целочисленным индексам»).
+
+# Используя train.iloc[-1] заменяем нулевой индекс в pred_previous
+# минус первым трейновой выборки, это и будет предыдущее значение.
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+data = pd.read_csv('/datasets/energy_consumption.csv', index_col=[0], parse_dates=[0])
+data.sort_index(inplace=True)
+data = data.resample('1D').sum()
+
+train, test = train_test_split(data, shuffle=False, test_size=0.2)
+
+print("Средний объём электропотребления в день:", test['PJME_MW'].mean())
+
+pred_previous = test.shift(fill_value=train['PJME_MW'].iloc[-1])
+mae = mean_absolute_error(pred_previous, test['PJME_MW'])
+print("MAE:", mae)
+
+# Output
+# Средний объём электропотребления в день: 745523.4529702971
+# MAE: 44941.65924092409
+
+
+
