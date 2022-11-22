7f6fc496024765c6c1aeb3f0c7257c451c4838e7
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-11-22 20:50:36 +0100

    22112022 commit

--- 1_linear_algebra/31_creating_features.py
+++ 1_linear_algebra/31_creating_features.py
@@ -27,3 +27,29 @@ print(data.head(10))
 # Скользящее среднее в моменте t учитывает текущее значение ряда x(t).
 # Это некорректно: целевой признак «убежал» в признаки. Вычисление
 # скользящего среднего не должно включать в себя текущее значение ряда.
+
+#### EXERCISE 1 ####
+
+# 1. Напишите функцию make_features() (англ. «создать признаки»),
+# чтобы прибавить к таблице четыре новых календарных признака:
+# год, месяц, день и день недели. Имена столбцов должны быть такие:
+# 'year', 'month', 'day', 'dayofweek'. Примените функцию к таблице
+# и напечатайте на экране её первые пять строк.
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('/datasets/energy_consumption.csv', index_col=[0], parse_dates=[0])
+data.sort_index(inplace=True)
+data = data.resample('1D').sum()
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.month.html
+def make_features(data):
+    data['year'] = data.index.year
+    data['month'] = data.index.month
+    data['day'] = data.index.day
+    data['dayofweek'] = data.index.dayofweek
+    return data
+
+make_features(data)
+print(data.head())
+
