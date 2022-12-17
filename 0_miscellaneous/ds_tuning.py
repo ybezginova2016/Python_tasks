0c8b934a0ec0f5f223f7ad3bf20ed5f60a13ca21
yuliabezginova <yulia.bezginova2016@gmail.com>
2022-12-17 16:30:07 +0100

    15122022 Linear Regression, Minimizing the loss function

---
+++ 0_miscellaneous/ds_tuning.py
@@ -0,0 +1,8 @@
+def find_max(nums):
+    max_num = float("-inf") # smaller than all other numbers
+    for num in nums:
+        if num > max_num:
+            max_num += 1 # (Fill in the missing line here)
+    return max_num
+
+print(find_max(5))
